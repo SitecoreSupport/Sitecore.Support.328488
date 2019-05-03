093a73b0e20f2a5f79cee02f472f05c99f586f55
iro <iro@sitecore.net>
2019-05-03 17:05:34 +0300

    Initial

---
+++ src/Sitecore.Support.328488/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.328488")]
+[assembly: AssemblyProduct("Sitecore.Support.328488")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.328488/RedirectMapResolver.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.XA.Feature.Redirects.Pipelines.HttpRequest;
+
+namespace Sitecore.Support.XA.Feature.Redirects.Pipelines.HttpRequest
+{
+  public class RedirectMapResolver : Sitecore.XA.Feature.Redirects.Pipelines.HttpRequest.RedirectMapResolver
+  {
+    protected override RedirectMapping FindMapping(string filePath)
+    {
+      foreach (var mapping in MappingsMap)
+      {
+        if ((!mapping.IsRegex && mapping.Pattern == filePath) || (mapping.IsRegex && mapping.Regex.IsMatch(filePath)))
+        {
+          return mapping;
+        }
+      }
+      return null;
+    }
+  }
+}
