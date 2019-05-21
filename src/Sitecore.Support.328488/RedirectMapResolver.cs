a148f218a135a4de1c6a9c9ffb3406dd138fa964
Anton Kechashin <atk@sitecore.net>
2019-05-21 17:01:37 +0300

    Updated fix

--- src/Sitecore.Support.328488/RedirectMapResolver.cs
+++ src/Sitecore.Support.328488/RedirectMapResolver.cs
@@ -6,9 +6,10 @@ namespace Sitecore.Support.XA.Feature.Redirects.Pipelines.HttpRequest
   {
     protected override RedirectMapping FindMapping(string filePath)
     {
+      char[] slash = { '/' };
       foreach (var mapping in MappingsMap)
       {
-        if ((!mapping.IsRegex && mapping.Pattern == filePath) || (mapping.IsRegex && mapping.Regex.IsMatch(filePath)) || (Context?.RawUrl != null && mapping.Pattern == Context.RawUrl.ToLower()))
+        if ((!mapping.IsRegex && mapping.Pattern == filePath) || (mapping.IsRegex && mapping.Regex.IsMatch(filePath)) || (Context?.RawUrl != null && mapping.Pattern.TrimEnd(slash) == Context.RawUrl.ToLower().TrimEnd(slash)))
         {
           return mapping;
         }
