d841a93123b2bc64c20bcddf94d438e685f1b0dd
DK\IEKA <ieka@sitecore.net>
2019-06-12 09:48:14 +0300

    Fix the issue with redirecting all pages to the English version.

--- src/Sitecore.Support.328488/RedirectMapResolver.cs
+++ src/Sitecore.Support.328488/RedirectMapResolver.cs
@@ -1,9 +1,18 @@
-﻿using Sitecore.XA.Feature.Redirects.Pipelines.HttpRequest;
+﻿using Sitecore.Pipelines.HttpRequest;
+using Sitecore.XA.Feature.Redirects.Pipelines.HttpRequest;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Caching;
 
 namespace Sitecore.Support.XA.Feature.Redirects.Pipelines.HttpRequest
 {
   public class RedirectMapResolver : Sitecore.XA.Feature.Redirects.Pipelines.HttpRequest.RedirectMapResolver
   {
+    private string ResolvedMappingsPrefix => "SXA-Redirect-ResolvedMappings-" + Context.Database.Name + "-" + Context.Site.Name;
+    private string AllMappingsPrefix => "SXA-Redirect-AllMappings-" + Context.Database.Name + "-" + Context.Site.Name;
+
     protected override RedirectMapping FindMapping(string filePath)
     {
       char[] slash = { '/' };
@@ -15,6 +24,49 @@ namespace Sitecore.Support.XA.Feature.Redirects.Pipelines.HttpRequest
         }
       }
       return null;
+    }
+
+    public override void Process(HttpRequestArgs args)
+    {
+      if (Context.Item != null || Context.Database == null || !Context.Site.IsSxaSite() || IsFile(Context.Request.FilePath))
+      {
+        return;
+      }
+      #region FIX bug #328488
+      string text = EnsureSlashes(this.Context.RawUrl.ToLower());
+      #endregion
+      RedirectMapping redirectMapping = GetResolvedMapping(text);
+      bool flag = redirectMapping != null;
+      if (redirectMapping == null)
+      {
+        redirectMapping = FindMapping(text);
+      }
+      if (redirectMapping != null && !flag)
+      {
+        Dictionary<string, RedirectMapping> dictionary = (HttpRuntime.Cache[ResolvedMappingsPrefix] as Dictionary<string, RedirectMapping>) ?? new Dictionary<string, RedirectMapping>();
+        dictionary[text] = redirectMapping;
+        HttpRuntime.Cache.Add(ResolvedMappingsPrefix, dictionary, null, DateTime.UtcNow.AddMinutes(CacheExpiration), TimeSpan.Zero, CacheItemPriority.Normal, null);
+      }
+      if (redirectMapping != null && HttpContext.Current != null)
+      {
+        string targetUrl = GetTargetUrl(redirectMapping, text);
+        if (redirectMapping.RedirectType == RedirectType.Redirect301)
+        {
+          Redirect301(HttpContext.Current.Response, targetUrl);
+        }
+        if (redirectMapping.RedirectType == RedirectType.Redirect302)
+        {
+          HttpContext.Current.Response.Redirect(targetUrl, endResponse: true);
+        }
+        if (redirectMapping.RedirectType == RedirectType.ServerTransfer)
+        {
+          HttpContext.Current.Server.TransferRequest(targetUrl);
+        }
+      }
+    }
+    private string EnsureSlashes(string text)
+    {
+      return StringUtil.EnsurePostfix('/', StringUtil.EnsurePrefix('/', text));
     }
   }
 }
